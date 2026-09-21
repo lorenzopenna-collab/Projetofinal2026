f5cb09a4f45eed337b0663c42457affdd7317e04
lorenzopenna-collab <lorenzo.penna@aluno.senai.br>
2026-09-21 17:42:08 -0300

    Delete sitee_escolaa2.0 directory

--- sitee_escolaa2.0/Api/src/routes/GradesRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const GradesController = require('../controllers/GradesController.js');
-
-const router = Router();
-
-// Listar boletim de notas
-router.get('/notas', GradesController.listarNotas);
-
-module.exports = router;
